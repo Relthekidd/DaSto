b8e65474c3259705b1be0ac2de8566ee281f410a
Relthekidd <relthekidd@gmail.com>
2024-12-21 09:51:18 -0500

    Delete src directory

--- src/constants/menu.ts
+++
@@ -1,8 +0,0 @@
-export const menuItems = [
-  { name: 'Flower', href: '#flower' },
-  { name: 'Carts', href: '#carts' },
-  { name: 'Edibles', href: '#edibles' },
-  { name: 'Pre-rolls', href: '#pre-rolls' },
-  { name: 'Concentrates', href: '#concentrates' },
-  { name: 'Accessories', href: '#accessories' },
-];
